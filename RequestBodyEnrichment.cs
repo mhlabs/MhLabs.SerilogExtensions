a5f7da43cda84e3e06b111f36b2fedd470ccfd5c
Stefan <stefan@successful.se>
2018-09-05 17:49:43 +0200

    feat: add request body enricher

---
+++ RequestBodyEnrichment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace MhLabs.SerilogExtensions
+{
+    public class RequestBodyEnrichment: ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var value = LogValueResolver.Resolve(this);
+            var property = new LogEventProperty("RequestBody", new ScalarValue(value));
+            logEvent.AddOrUpdateProperty(property);
+        }
+    }
+}
