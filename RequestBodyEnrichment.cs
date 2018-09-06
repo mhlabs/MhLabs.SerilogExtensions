cc18389b469626388ba3244b4eff34a976c77814
Stefan <stefan@successful.se>
2018-09-06 16:03:12 +0200

    fix: decode incomming base64 string

--- RequestBodyEnrichment.cs
+++ RequestBodyEnrichment.cs
@@ -1,17 +1,21 @@
 ﻿using System;
+using System.Text;
 using Serilog.Core;
 using Serilog.Events;
 
 namespace MhLabs.SerilogExtensions
 {
-    public class RequestBodyEnrichment: ILogEventEnricher
+    public class RequestBodyEnrichment : ILogEventEnricher
     {
         public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
         {
             var value = LogValueResolver.Resolve(this);
-            if (!string.IsNullOrWhiteSpace(value)) 
+            if (!string.IsNullOrWhiteSpace(value))
             {
-                var property = new LogEventProperty("RequestBody", new ScalarValue(value));
+                var bytes = Convert.FromBase64String(value);
+                var body = Encoding.UTF8.GetString(bytes);
+
+                var property = new LogEventProperty("RequestBody", new ScalarValue(body));
                 logEvent.AddOrUpdateProperty(property);
             }
         }
