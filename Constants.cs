175b4c665c5f499444d28c0e5c37502fe7e0e9c1
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2018-11-16 13:04:13 +0100

    feat: add correlation-id key

--- Constants.cs
+++ Constants.cs
@@ -3,5 +3,6 @@
     public static class Constants
     {
         public const string OutputTemplateFormat = "[{Level:u3}] [{Properties:j}] {Message:lj}{Exception}{NewLine}";
+        public const string CorrelationIdHeader = "mh-correlation-id";
     }
 }
