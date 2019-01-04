2d066756f3ad13879ca91969d188e5e039216089
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2019-01-04 16:11:51 +0100

    feat: add RequestPathEnrichment & UserAudienceEnrichment

---
+++ RequestPathEnrichment.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace MhLabs.SerilogExtensions
+{
+    public class RequestPathEnrichment : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var value = LogValueResolver.Resolve(this);
+            var property = new LogEventProperty("RequestPath", new ScalarValue(value));
+            logEvent.AddOrUpdateProperty(property);
+        }
+    }
+}
---
+++ UserAudienceEnrichment.cs
@@ -0,0 +1,15 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+
+namespace MhLabs.SerilogExtensions
+{
+    public class UserAudienceEnrichment : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory propertyFactory)
+        {
+            var value = LogValueResolver.Resolve(this);
+            var property = new LogEventProperty("UserAudience", new ScalarValue(value));
+            logEvent.AddOrUpdateProperty(property);
+        }
+    }
+}
