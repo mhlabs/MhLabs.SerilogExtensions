3f6f1240a07cae9563324d26c471935ad21f6996
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2019-09-23 17:25:58 +0200

    feat: remove alpha version from 1.4.0

--- MhLabs.SerilogExtensions/Constants.cs
+++ MhLabs.SerilogExtensions/Constants.cs
@@ -4,6 +4,6 @@
     {
         public const string OutputTemplateFormat = "[{Level:u3}] [{Properties:j}] {Message:lj}{Exception}{NewLine}";
         public const string CorrelationIdHeader = "mh-correlation-id";
-        public const string RouteTemplateHeader = "mh-routetemplate";
+        public const string RouteTemplateHeader = "routetemplate";
     }
 }
